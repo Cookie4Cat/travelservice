24c54579ef3ad468186f1294d2e6fd3313515d3a
Cookie4Cat <guozy42@foxmail.com>
2016-09-07 11:08:33 +0800

    删除以备之后重新生成

--- src/edu/ynu/travel/service/complaint/ITravelerService.java
+++ src/edu/ynu/travel/service/complaint/ITravelerService.java
@@ -1,9 +1,5 @@
 package edu.ynu.travel.service.complaint;
 
-import edu.ynu.travel.entity.complaint.ComplaintEntity;
-
-import java.util.List;
-
 public interface ITravelerService {
-    List<ComplaintEntity> getComplainsByTID(Integer tid);
+
 }
--- src/edu/ynu/travel/service/complaint/impl/TravelerService.java
+++ src/edu/ynu/travel/service/complaint/impl/TravelerService.java
@@ -1,22 +1,10 @@
 package edu.ynu.travel.service.complaint.impl;
 
-import edu.ynu.travel.entity.complaint.ComplaintEntity;
-import edu.ynu.travel.entity.complaint.ComplaintEntityExample;
-import edu.ynu.travel.mapper.complaint.ComplaintEntityMapper;
+
 import edu.ynu.travel.service.complaint.ITravelerService;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.util.List;
 @Service
 public class TravelerService implements ITravelerService {
-    @Resource(name = "complaintMapper")
-    private ComplaintEntityMapper complaintEntityMapper;
-    @Override
-    public List<ComplaintEntity> getComplainsByTID(Integer tid) {
-        ComplaintEntityExample example = new ComplaintEntityExample();
 
-        complaintEntityMapper.selectByExample(example);
-        return null;
-    }
 }
