b02113929cd2f844189b6e3cfe5dd4a12e51ce89
Cookie4Cat <guozy42@foxmail.com>
2016-09-27 00:53:53 +0800

    完成游记app接口

---
+++ src/edu/ynu/travel/controller/article/ATravellerController.java
@@ -0,0 +1,21 @@
+package edu.ynu.travel.controller.article;
+
+import edu.ynu.travel.message.article.ArticleList;
+import edu.ynu.travel.service.article.IArticleService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value = "/article/traveller")
+public class ATravellerController {
+    @Resource
+    private IArticleService articleService;
+
+    @RequestMapping(value = "/articles", method = RequestMethod.GET)
+    public ArticleList getAllArticle(@RequestParam(name = "page") int page,
+                                     @RequestParam(name = "size") int size){
+        return articleService.listArtcles(page,size);
+    }
+}
